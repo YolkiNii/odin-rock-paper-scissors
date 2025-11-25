834497b9d236b7830a777ed5f850988d226a357b
Youki Lewis <54418235+YolkiNii@users.noreply.github.com>
2025-11-25 08:12:50 -0600

    feat: simulate a round

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const computerChoice = Math.floor(Math.random() * 3);
     let outcome = "";
@@ -25,6 +28,39 @@ function getHumanChoice() {
     return humanChoice;
 }
 
+function playRound(humanChoice, computerChoice) {
+    const humanChoiceLowered = humanChoice.toLowerCase();
+
+    if (humanChoiceLowered === computerChoice) {
+        console.log(
+            `You and the computer both picked "${humanChoiceLowered}". It's a draw!`
+        );
+        return;
+    }
+
+    if (
+        (humanChoiceLowered === "rock" && computerChoice === "scissors") || 
+        (humanChoiceLowered === "scissors" && computerChoice === "paper") ||
+        (humanChoiceLowered === "paper" && computerChoice === "rock")
+    ) {
+        console.log(
+            `You won! You chose ${humanChoiceLowered}, the computer chose ${computerChoice}.`
+        );
+    }
+    else {
+        console.log(
+            `You lost! You chose ${humanChoiceLowered}, the computer chose ${computerChoice}.`
+        )
+    }
+
+    return;
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
 console.log("Hello World");
-console.log(getHumanChoice())
-console.log(getComputerChoice())
+console.log(humanSelection);
+console.log(computerSelection);
+
+playRound(humanSelection, computerSelection);
