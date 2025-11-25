c6d9e9d1e21987ecd0f1e71bc557d35aab841679
Youki Lewis <54418235+YolkiNii@users.noreply.github.com>
2025-11-25 06:20:24 -0600

    feat: add random computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,24 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+    const choice = Math.floor(Math.random() * 3)
+    let outcome = ""
+
+    switch(choice) {
+        case 0:
+            outcome = "rock"
+            break
+        case 1:
+            outcome = "paper"
+            break
+        case 2:
+            outcome = "scissors"
+            break
+        default:
+            outcome = "rock"
+    }
+
+    return outcome
+}
+
+console.log(getComputerChoice())
